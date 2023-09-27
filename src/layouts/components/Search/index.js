466ba34db610f0305b34f6dd49d438b413908cf0
dothihongkhanh <dothihongkhanh22@gmail.com>
2023-09-27 19:55:56 +0700

    optimizing code 1

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -5,7 +5,7 @@ import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 import classNames from 'classnames/bind';
 
-import * as searchServices from '~/apiServices/searchServices';
+import * as searchServices from '~/services/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
